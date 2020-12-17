0b14f54d4285651ecfb8b526ad03844a6c16d7af
Shkredov Dmitriy <ShkredovDmitriy@gmail.com>
2020-12-17 17:37:55 +0300

    Fix bug

--- src/components/ds-modal/_lib/_modalApiSetConfig.ts
+++ src/components/ds-modal/_lib/_modalApiSetConfig.ts
@@ -2,25 +2,24 @@ import config from "./_config";
 import { message } from "./_log";
 import modalChangeZindex from "./_modalChangeZindex";
 
-export default function configSet({...data}) {
-    if (data.logs === true) {
-      config.logs = true;
-      message(`${config.logComponent}: logs = true`, config.logs);
-    }
-    if (data.logs === false) {
-      config.logs = false;
-      message(`${config.logComponent}: logs = false`, config.logs);
-    }      
-    if (data.outClick === true) {
-      config.outClick = true;
-      message(`${config.logComponent}: outClick = true`, config.logs);
-    }
-    if (data.outClick === false) {
-      config.outClick = false;
-      message(`${config.logComponent}: outClick = false`, config.logs);
-    }
-    if (data.zIndex) {
-      modalChangeZindex(data.zIndex);
-    }
-   
+export default function modalApiSetConfig({...data}) {
+  if (data.logs === true) {
+    config.logs = true;
+    message(`${config.logComponent}: logs = true`, config.logs);
+  }
+  if (data.logs === false) {
+    config.logs = false;
+    message(`${config.logComponent}: logs = false`, config.logs);
+  }      
+  if (data.outClick === true) {
+    config.outClick = true;
+    message(`${config.logComponent}: outClick = true`, config.logs);
+  }
+  if (data.outClick === false) {
+    config.outClick = false;
+    message(`${config.logComponent}: outClick = false`, config.logs);
+  }
+  if (data.zIndex) {
+    modalChangeZindex(data.zIndex);
+  }
 }
